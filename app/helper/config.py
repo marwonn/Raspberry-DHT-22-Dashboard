a2473578c75f174dcfa416273d13a8f170174706
marwonn <marc@wonneberger.xyz>
2021-09-16 11:30:11 +0200

    .

---
+++ app/helper/config.py
@@ -0,0 +1,11 @@
+class Config:
+
+    '''SSH Configuration'''
+    HOSTNAME = "192.168.xxx.xx"
+    USERNAME = "pi"
+    PASSWORD = "YOUR_password"
+
+    '''Path Configuration'''
+    PATH_2_DB = "path/to/database/on/Raspi/Zero"
+    PATH_2_DB_TARGET_LOCATION = "path/to/database/on/Raspi/4B"
+    
